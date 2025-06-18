12b20504ae1104e0d8ca9859bbd132801d6028b8
gerardocrr <gerardo2551.cr@gmail.com>
2025-06-17 18:10:58 -0600

    fix: update socket connection URL to use environment variable

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -1,7 +1,5 @@
 import { io } from "socket.io-client";
 
-const URL = "http://localhost:3000";
+const URL = import.meta.env.VITE_URL ?? "http://localhost:3000";
 
-export const socket = io(URL, {
-  autoConnect: false,
-});
+export const socket = io(URL);
